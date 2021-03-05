f7d6da8db3f1e0ca41160c5aef02938f67f2aa15
jtr109 <conbas2019@gmail.com>
2021-03-05 15:54:35 +0800

    feat(stack): add push and pop

--- src/stack.rs
+++ src/stack.rs
@@ -10,9 +10,38 @@ struct Node<T> {
 }
 
 impl<T> Stack<T> {
-    fn new(elem: T) -> Self {
-        Stack {
-            head: Some(Box::new(Node { elem, next: None })),
-        }
+    pub fn new() -> Self {
+        Stack { head: None }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        self.head = Some(Box::new(Node {
+            elem,
+            next: self.head.take(),
+        }));
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|boxed_node| {
+            self.head = boxed_node.next;
+            boxed_node.elem
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        let mut stack = Stack::new();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        assert_eq!(stack.pop(), Some(3));
+        assert_eq!(stack.pop(), Some(2));
+        assert_eq!(stack.pop(), Some(1));
+        assert_eq!(stack.pop(), None);
     }
 }
