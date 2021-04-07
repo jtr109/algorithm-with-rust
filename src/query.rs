e944744806c4e1b046984f3e49edf480f944d445
Ryan Li <conbas2019@gmail.com>
2021-04-07 22:11:37 +0800

    feat: add st trait

---
+++ src/query.rs
@@ -0,0 +1,9 @@
+trait ST<T, U> {
+    fn put(&mut self, key: T, value: U);
+    fn get(&self, key: T) -> Option<U>;
+    fn delete(&mut self, key: T);
+    fn contains(&self, key: T) -> bool;
+    fn is_empty(&self, key: T) -> bool;
+    fn size(&self) -> usize;
+    fn keys(&self) -> Vec<T>;
+}
