dce1c39f1cf716a5a0012c18add38b10d5f749c5
Ryan Li <conbas2019@gmail.com>
2021-04-26 22:24:52 +0800

    feat(digraph): add method dfs

--- src/directed_dfs.rs
+++ src/directed_dfs.rs
@@ -9,11 +9,30 @@ pub struct DirectedDFS<'a> {
 
 impl<'a> DirectedDFS<'a> {
     pub fn new(graph: &'a Digraph, sources: Vec<usize>) -> Self {
-        Self {
+        let mut dfs = Self {
             sources,
             marked: vec![false; graph.vertex()],
             edge_to: vec![None; graph.vertex()],
             graph,
+        };
+        for v in dfs.sources.clone() {
+            dfs.dfs(v);
+        }
+        dfs
+    }
+
+    fn marked(&self, v: usize) -> bool {
+        return self.marked[v];
+    }
+
+    fn dfs(&mut self, v: usize) {
+        self.marked[v] = true;
+        for w in self.graph.adj(v) {
+            if self.marked(w) {
+                continue;
+            }
+            self.edge_to[w] = Some(v);
+            self.dfs(w);
         }
     }
 }
