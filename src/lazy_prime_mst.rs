a996f446144c5f5f1fd5c731025efae75bed9636
Ryan Li <conbas2019@gmail.com>
2021-05-01 22:05:53 +0800

    feat(mst): add error message

--- src/lazy_prime_mst.rs
+++ src/lazy_prime_mst.rs
@@ -22,3 +22,26 @@ impl<'a> LazyPrimeMST<'a> {
         }
     }
 }
+
+// ❯ cargo build
+//    Compiling algorithms v0.1.0 (/Users/xxx/projects/learn/algorithm-with-rust)
+// error[E0597]: `e` does not live long enough
+//   --> src/lazy_prime_mst.rs:21:28
+//    |
+// 13 | impl<'a> LazyPrimeMST<'a> {
+//    |      -- lifetime `'a` defined here
+// ...
+// 21 |             self.pq.insert(&e);
+//    |             ---------------^^-
+//    |             |              |
+//    |             |              borrowed value does not live long enough
+//    |             argument requires that `e` is borrowed for `'a`
+// 22 |         }
+//    |         - `e` dropped here while still borrowed
+//
+// error: aborting due to previous error
+//
+// For more information about this error, try `rustc --explain E0597`.
+// error: could not compile `algorithms`
+//
+// To learn more, run the command again with --verbose.
