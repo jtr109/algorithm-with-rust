6d5aeb758dc16e67a0d4265ab2e63ce1d4770a80
Ryan Li <conbas2019@gmail.com>
2021-05-01 17:50:20 +0800

    feat(graph): add edge struct

--- src/edge_weigthed_graph.rs
+++ src/edge_weigthed_graph.rs
@@ -3,17 +3,13 @@ use std::cmp::Ordering;
 #[derive(Eq)]
 pub struct Edge {
     weight: usize,
-    either: usize,
-    other: usize,
+    v: usize,
+    w: usize,
 }
 
 impl Edge {
     fn new(v: usize, w: usize, weight: usize) -> Self {
-        Self {
-            weight,
-            either: v,
-            other: w,
-        }
+        Self { weight, v, w }
     }
 
     // 边的权重
@@ -23,12 +19,18 @@ impl Edge {
 
     // 边两端的顶点之一
     fn either(&self) -> usize {
-        self.either
+        self.v
     }
 
     // 另一个顶点
-    fn other(&self) -> usize {
-        self.other
+    fn other(&self, v: usize) -> usize {
+        if self.v == v {
+            self.w
+        } else if self.w == v {
+            self.v
+        } else {
+            panic!("Inconsistent edge")
+        }
     }
 }
 
@@ -50,10 +52,10 @@ impl PartialEq for Edge {
     }
 }
 
-trait EdgeWeightedGraphAPI {
-    fn vertex_count(&self) -> usize;
-    fn edge_count(&self) -> usize;
-    fn add_edge(&self, e: Edge) -> usize;
-    fn adj(&self, v: usize) -> Vec<Edge>;
-    fn edges(&self) -> Vec<Edge>;
-}
+// struct EdgeWeightedGraph {
+//     fn vertex_count(&self) -> usize;
+//     fn edge_count(&self) -> usize;
+//     fn add_edge(&self, e: Edge) -> usize;
+//     fn adj(&self, v: usize) -> Vec<Edge>;
+//     fn edges(&self) -> Vec<Edge>;
+// }
